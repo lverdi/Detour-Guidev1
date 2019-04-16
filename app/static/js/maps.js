c676e63567c43c7a03146ea5c2f6c8f79ec231e8
Jesse Salazar <js2928@cornell.edu>
2019-04-15 20:56:43 -0400

    added detour time and distance

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -5,56 +5,61 @@ var mapElement;
 var map;
 var origin;
 var destination;
+var initialDistance;
+var initialTime;
 
 function initMap() {
-    var bounds  = new google.maps.LatLngBounds();
-    directionsRenderer = new google.maps.DirectionsRenderer;
-    directionsService = new google.maps.DirectionsService;
-    mapElement = document.getElementById('map');
-    origin = coords[0];
-    destination = coords[1];
+  var bounds = new google.maps.LatLngBounds();
+  directionsRenderer = new google.maps.DirectionsRenderer;
+  directionsService = new google.maps.DirectionsService;
+  mapElement = document.getElementById('map');
+  origin = coords[0];
+  destination = coords[1];
 
-    map = new google.maps.Map(mapElement, {
-      zoom: 7,
-      center: origin
-    });
-    directionsRenderer.setMap(map);
-    directionsService.route({
-      origin: origin,
-      destination: destination,
-      travelMode: "DRIVING"
-    }, function(response, status) { 
-      if (status == 'OK') {
-        directionsRenderer.setDirections(response);
-        var tt = 0;
-        var td = 0;
-        response["routes"][0]["legs"].forEach(function(leg, i) {
-          tt += leg["duration"]["value"];
-          td += leg["distance"]["value"];
-        })
-        document.getElementById("time").innerHTML = "Total time : " + (tt / 3600).toFixed(2) + " hours"
-        document.getElementById("distance").innerHTML = "Total distance : " + (td / 1609.34).toFixed(1) + " miles"
-
-      } else if (status == 'ZERO_RESULTS'){ 
-        // catch error
-        console.log("There are no results for this route.")
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
+  map = new google.maps.Map(mapElement, {
+    zoom: 7,
+    center: origin
+  });
+  directionsRenderer.setMap(map);
+  directionsService.route({
+    origin: origin,
+    destination: destination,
+    travelMode: "DRIVING"
+  }, function (response, status) {
+    if (status == 'OK') {
+      directionsRenderer.setDirections(response);
+      var tt = 0;
+      var td = 0;
+      response["routes"][0]["legs"].forEach(function (leg, i) {
+        tt += leg["duration"]["value"];
+        td += leg["distance"]["value"];
+      })
+      initialTime = tt;
+      initialDistance = td;
+      var hrs = Math.floor(tt / 3600);
+      var min = ((tt / 3600) - hrs) * 60;
+      document.getElementById("time").innerHTML = "Total time: " + hrs + " hours " + Math.floor(min) + " minutes"
+      document.getElementById("distance").innerHTML = "Total distance: " + (td / 1609.34).toFixed(1) + " miles"
+    } else if (status == 'ZERO_RESULTS') {
+      // catch error
+      console.log("There are no results for this route.")
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
 
-function updateMap(lat=null, long=null) {
+function updateMap(lat = null, long = null) {
   if (lat == null && long == null) {
     // Reset button clicked
     wypts = []
   } else {
     wypts = [{
-      location : {
-        lat : lat,
-        lng : long
-      }, 
-      stopover : true
+      location: {
+        lat: lat,
+        lng: long
+      },
+      stopover: true
     }]
   }
   var mapElement = document.getElementById('map');
@@ -63,16 +68,32 @@ function updateMap(lat=null, long=null) {
     destination: destination,
     waypoints: wypts,
     travelMode: "DRIVING"
-  }, function(response, status) {
+  }, function (response, status) {
     if (status == 'OK') {
       var tt = 0;
-        var td = 0;
-        response["routes"][0]["legs"].forEach(function(leg, i) {
-          tt += leg["duration"]["value"];
-          td += leg["distance"]["value"];
-        })
-        document.getElementById("time").innerHTML = "Total time : " + (tt / 3600).toFixed(2) + " hours"
-        document.getElementById("distance").innerHTML = "Total distance : " + (td / 1609.34).toFixed(1) + " miles"
+      var td = 0;
+      response["routes"][0]["legs"].forEach(function (leg, i) {
+        tt += leg["duration"]["value"];
+        td += leg["distance"]["value"];
+      })
+      var changeTime = tt - initialTime;
+      var changeHrs = Math.floor(changeTime / 3600);
+      var changeMin = Math.floor(((changeTime / 3600) - changeHrs) * 60);
+
+      var changeDist = td - initialDistance;
+
+      var hrs = Math.floor(tt / 3600);
+      var min = ((tt / 3600) - hrs) * 60;
+
+      if (lat == null && long == null) {
+        document.getElementById("time").innerHTML = "Total time: " + hrs + " hours " + Math.floor(min) + " minutes"
+        document.getElementById("distance").innerHTML = "Total distance: " + (td / 1609.34).toFixed(1) + " miles"
+      }
+      else {
+        document.getElementById("time").innerHTML = "Total time: " + hrs + " hours " + Math.floor(min) + " minutes ( " + changeHrs + " hour " + changeMin + " minute detour )"
+        document.getElementById("distance").innerHTML = "Total distance: " + (td / 1609.34).toFixed(1) + " miles ( " + (changeDist / 1609.34).toFixed(1) + " mile detour )"
+      }
+
       directionsRenderer.setDirections(response);
     } else {
       window.alert('Directions request failed due to ' + status);
